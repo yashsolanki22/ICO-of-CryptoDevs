e029aa9c5b9fd217fbdc08dfefeb76a53eaa2ac1
Yash solanki <47449493+yash-solanki069@users.noreply.github.com>
2022-02-05 15:55:10 +0530

    Add files via upload

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x8E8e795089d45320fE89E3d8D58091973bfF8e29";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
